462b25142e0b0f2b49e8ebcad4946cd8607ed735
Ruben Paixão <rpaixao@opensoft.pt>
2017-02-21 23:44:26 +0000

    Alterada ordem dos TODOs do checkpoint 1

--- checkpoints-src/checkpoint1-init/app/OpentourAPP.js
+++ checkpoints-src/checkpoint1-init/app/OpentourAPP.js
@@ -4,13 +4,13 @@
 
 //TODO 3: Criar classe do componente OpentourApp
 
-
-//TODO 7: Definir callback onPress
-//TODO 8: Correr comando react-native log-android ou log-ios para ver os logs na consola
-
 // TODO 4: Definir método Render com elemento View do componente com um elemento textual
-// TODO 6: Utilizar estilo bigTitle
-// TODO 7: Alterar View do componente para usar um botão fornecido pelo React-native (TouchableHighlight
 
+// TODO 6: Utilizar estilo bigTitle
+
+// TODO 7:
+//    - Alterar View do componente para usar um botão fornecido pelo React-native (TouchableHighlight
+//    - Definir callback onPress
+//    - Correr comando react-native log-android ou log-ios para ver os logs na consola
 
 //TODO 5: Exportar o componente OpentourApp para poder ser invocado por outros componentes
